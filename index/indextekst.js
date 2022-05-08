27aaccaae94ebdf46b10ca12f019593101cbf26f
MyAccountJan <88849245+MyAccountJan@users.noreply.github.com>
2022-05-08 13:38:58 +0200

    Add files via upload

---
+++ index/indextekst.js
@@ -0,0 +1,8 @@
+var color = ["#222f3e","#f368e0","#ee5253","#0abde3","#10ac84","#222f3e","#5f27cd","#fff","#191919"];
+var i = 0;
+document.querySelector("button").addEventListener("click",function(){
+    i = 1 < color.length ? ++i : 0;
+document.querySelector("body").style.background = color[i]
+});
+
+// Zmiana kolorów tła
